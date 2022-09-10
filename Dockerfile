0807dcf11ef80013d532867d9525a89b48641f98
r3-endo <endo.ryo.mm@gmail.com>
2022-09-10 12:45:12 +0900

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:14.17.1-alpine
+ENV LANG=C.UTF-8
+ENV TZ=Asia/Tokyo
+
+WORKDIR /usr/src/react-chart-js
---
+++ frontend/src/App.tsx
@@ -0,0 +1,62 @@
+
+import {
+  Chart as ChartJS,
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
+
+ChartJS.register(
+  LinearScale,
+  CategoryScale,
+  BarElement,
+  PointElement,
+  LineElement,
+  Legend,
+  Tooltip,
+  LineController,
+  BarController
+);
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      type: 'line' as const,
+      label: 'Dataset 1',
+      borderColor: 'rgb(255, 99, 132)',
+      borderWidth: 2,
+      fill: false,
+      data: labels.map(() => 5),
+    },
+    {
+      type: 'bar' as const,
+      label: 'Dataset 2',
+      backgroundColor: 'rgb(75, 192, 192)',
+      data: labels.map(() => 10),
+      borderColor: 'white',
+      borderWidth: 2,
+    },
+    {
+      type: 'bar' as const,
+      label: 'Dataset 3',
+      backgroundColor: 'rgb(53, 162, 235)',
+      data: labels.map(() => 8),
+    },
+  ],
+};
+
+export function App() {
+  return <Chart type='bar' data={data} />;
+}
+
+export default App;
